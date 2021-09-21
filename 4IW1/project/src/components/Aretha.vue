e3a381e2839d1e64bc92462942f2e73b2aeb032d
Karl MARQUES <kmarques@vetixy.com>
2021-09-21 20:35:51 +0200

    4IW1: data + structure

---
+++ 4IW1/project/src/components/Aretha.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>{{ fullname }}</h1>
+</template>
+
+<script>
+export default {
+  name: "Aretha",
+  data: () => ({
+    lastname: "Franklin",
+    firstname: "Aretha",
+  }),
+  computed: {
+    fullname: function () {
+      return [this.lastname, this.firstname].join(" ");
+    },
+  },
+  watch: {
+    fullname: function (value) {
+      alert(value);
+    },
+  },
+};
+</script>
---
+++ 4IW1/project/src/components/ToggleButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button v-on:click="checked = !checked">
+    {{ title }}
+    {{ yatitle }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ToggleButton",
+  data: () => ({
+    checked: false,
+    yatitle: "Unchecked",
+  }),
+  computed: {
+    title: function () {
+      return this.checked ? "Checked" : "Unchecked";
+    },
+  },
+  watch: {
+    title: function (val) {
+      this.yatitle = val;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
