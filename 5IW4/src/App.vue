e9b2fff64d4118c3bc5371bc3d4de6043a21dec0
Karl MARQUES <kmarques@vetixy.com>
2022-10-10 13:04:32 +0200

    5IW4: create app + template + props + event listener + bind

---
+++ 5IW4/src/App.vue
@@ -0,0 +1,108 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+import Button from "./components/Button.vue";
+import { ref } from "vue";
+
+const isRed = ref(false);
+console.log("isRed", isRed, isRed.value);
+
+function toggleTheme() {
+  isRed.value = !isRed.value;
+}
+</script>
+
+<!--
+  Vue Composition API (en mode Options)
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TheWelcome
+  },
+  async setup() {
+    return {
+    }
+  }
+}
+</script>
+-->
+
+<!--
+  Vue Options API
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TheWelcome
+  }
+}
+</script>
+-->
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div :class="['wrapper', { 'bg-red': isRed }]">
+      <p>Theme is red: {{ isRed }}</p>
+      <Button title="Toggle Red theme" color="red" :onClick="toggleTheme" />
+      <Button title="<table><tr>Hello 2</tr></table>" />
+      <Button title="Hello 2" v-bind:rounded="true" />
+      <HelloWorld msg="You did At!" id="4" msg2="test" />
+      <HelloWorld msg="You did It!" />
+      <HelloWorld msg="You did Up!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+
+.bg-red {
+  background-color: red;
+}
+</style>
---
+++ 5IW4/src/components/Button.vue
@@ -0,0 +1,88 @@
+<script setup>
+const props = defineProps({
+  onClick: {
+    type: Function,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  rounded: {
+    type: Boolean,
+    default: false,
+  },
+  color: {
+    type: String,
+    default: "blue",
+    validator: (value) => {
+      return ["blue", "red", "green"].includes(value);
+    },
+  },
+});
+
+function handleClick(type, event) {
+  console.log(event);
+  console.log("click");
+  props.onClick();
+}
+
+console.log(props);
+</script>
+
+<!--
+    Options API
+<script>
+export default {
+    name: 'Button',
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        rounded: {
+            type: Boolean,
+            default: false,
+        },
+        color: {
+            type: String,
+            default: 'blue',
+            validator: (value) => {
+                return ['blue', 'red', 'green'].includes(value);
+            },
+        },
+    },
+    methods: {
+        handleClick() {
+            console.log('click');
+        },
+    },
+}
+</script>
+-->
+
+<template>
+  <button
+    :style="{
+      color,
+      borderRadius: rounded ? '50%' : '0',
+    }"
+    @click="handleClick('test', $event)"
+  >
+    {{ title }}
+  </button>
+</template>
+
+<!--
+<template>
+    <button
+      v-bind:style="{
+        color,
+        borderRadius: rounded ? '50%' : '0',
+      }"
+      v-on:click="handleClick('test', $event)"
+    >
+      {{ title }}
+    </button>
+  </template>
+  -->
