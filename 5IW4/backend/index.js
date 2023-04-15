1dcc7f2596696a77d9bd4035457645d6fbc81d7a
Karl MARQUES <kmarques@vetixy.com>
2023-04-15 17:41:11 +0200

    5IW4: tips backend

---
+++ 5IW4/backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const tracker = require("./middleware/tracker");
+const app = express();
+
+app.use(cors());
+
+// Logging : winston + morgan
+app.use(
+  tracker({
+    APP_ID: "5IW4",
+  })
+);
+
+app.get("/", (req, res, next) => {
+  req.sendTrackingEvent({
+    tag: "home",
+    event_type: "pageview",
+  });
+  res.json({ msg: "This is CORS-enabled for all origins!" });
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
